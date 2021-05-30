5fae35939c810fbc3548da6a5d7c3afeed7d4791
Ilham <ivannurilham60@gmail.com>
2021-05-30 14:36:35 +0700

    add base color and textStyle

---
+++ lib/styles.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+class KColors {
+  static const Color primaryColor = Color(0xFFFDC521);
+  static const Color textColor = Color(0xFF22242B);
+  static const Color darkBlue = Color(0xFF2e3B72);
+  static const Color grey = Color(0xFFEBEBEB);
+  static const Color red = Color(0xFFED000E);
+}
+
+class KText {
+  static TextStyle title = TextStyle(
+    fontSize: 24,
+    color: KColors.primaryColor,
+    fontWeight: FontWeight.bold
+  );
+
+  static TextStyle normal = TextStyle(
+    fontSize: 18,
+    color: KColors.primaryColor,
+    fontWeight: FontWeight.w600
+  );
+
+  static TextStyle small = TextStyle(
+    fontSize: 16,
+    color: KColors.primaryColor,
+    fontWeight: FontWeight.normal
+  );
+}
